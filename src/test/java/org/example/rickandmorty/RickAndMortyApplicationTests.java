08349c86bfb1186065f5c9aaed9b4134b3cf50c8
Ole Backhaus <ole.backhaus@gmx.de>
2024-10-25 11:28:35 +0200

    Initial commit

---
+++ src/test/java/org/example/rickandmorty/RickAndMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rickandmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
